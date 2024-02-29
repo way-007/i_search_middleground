c1ee205db2060a7b471e88020bed7127970b4786
way-007 <194648458@qq.com>
2024-02-29 20:56:43 +0800

    删除前端代码

--- fronted/yusou_fronted/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "ant-design-vue/dist/reset.css";
-import Antd from "ant-design-vue";
-
-const app = createApp(App);
-
-app.use(Antd).use(router).mount("#app");
